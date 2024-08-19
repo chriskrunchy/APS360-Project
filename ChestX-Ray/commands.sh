0b3a6ea29d58e06e04bda806ce56a0a62d671aca
adamroberge <adamrocket33@gmail.com>
2024-08-19 05:57:01 +0000

    Add resnet50 and resnet152 for further testing

--- ChestX-Ray/commands.sh
+++ ChestX-Ray/commands.sh
@@ -1,6 +1,9 @@
 # pretrained resnet
 python pretrained_resnet18.py --batch_size 32 --num_workers 4 --gpus "0,1" --num_epochs 100
 python pretrained_resnet34.py --batch_size 32 --num_workers 4 --gpus "2,3" --num_epochs 100
+python pretrained_resnet50.py --batch_size 32 --num_workers 4 --gpus "0,1" --num_epochs 100
+python pretrained_resnet152.py --batch_size 32 --num_workers 4 --gpus "0,1" --num_epochs 100
+
 
 # efficientNet (pretrained)
 python pretrained_efficientnet.py --batch_size 32 --num_workers 4 --gpus "0,1" --num_epochs 100
